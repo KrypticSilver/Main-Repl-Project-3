4cf4f2417a8d26c4f2b51507427f5c6c94b85e94
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-17 16:10:46 +0000

    restructured

--- .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
+++ .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
@@ -3,29 +3,13 @@
 
 using namespace std;
 
-int fib(int n) {
-  int sum = 0;
-  int a = 0;
-  int b = 1;
-  int c = 1;
-
-  for (int i = 0; i < n; i++) {
-    a = b;
-    b = c;
-    c = a + b;
-
-    sum += c;
-    
-    
-  }
-
-  return sum;
-}
-
 int main() {
+  string sentence = "Hello, my name is Joey Brigaleous";
 
-  int n 2 ;
+  int key = 1851;
 
-  cout << fib(n) << fib(n + 1) << fib(n + 2);
-
+  for (int i = 0; i < sentence.size(); i++) {
+    sentence[i] = sentence[i]
+  }
+  
 }
--- main.cpp
+++ main.cpp
@@ -3,26 +3,13 @@
 
 using namespace std;
 
-int fib(int n) {
-  n = n - 2;
-  
-  int a = 0;
-  int b = 1;
-  int c = 1;
-
-  for (int i = 0; i < n; i++) {
-    a = b;
-    b = c;
-    c = a + b;
-  }
-
-  return c;
-}
-
 int main() {
+  string sentence = "Hello, my name is Joey Brigaleous";
 
-  int n = 5;
+  int key = 1851;
 
-  cout << fib(n);
-
+  for (int i = 0; i < sentence.size(); i++) {
+    sentence[i] = sentence[i]
+  }
+  
 }
