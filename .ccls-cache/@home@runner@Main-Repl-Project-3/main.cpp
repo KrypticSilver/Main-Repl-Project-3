d5d4938347a2370cffd0b91e035622e601ecae8c
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-17 23:42:59 +0000

    worked on asymetrical encryption, primes generation

--- .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
+++ .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main() {
-  string sentence = "Hello, my name is Joey Brigaleous";
 
-  int key = 1851;
 
-  for (int i = 0; i < sentence.size(); i++) {
-    sentence[i] = sentence[i];
+vector<int> gen_primes(int range_ub, int range_lb) {
+  
+  vector<int> primes;
+  
+  for (int i = range_lb; i < range_ub; i++) {
+    vector<int> factors;
+    
+    for (int j = 1; j <= i; j++) {
+      
+      if (i % j == 0) {
+        factors.push_back(j);
+      }
+    }
+
+    if (factors.size() == 2) {
+      primes.push_back(i);
+      
+    }
+    factors = {};
   }
+
+  return primes;
+}
+
+int main() {
+  srand ( time(NULL) );
+  
+  int random = (1 + rand() % 10) * 5;
+  cout << random;
+  cout << "working!";
+  
   
 }
