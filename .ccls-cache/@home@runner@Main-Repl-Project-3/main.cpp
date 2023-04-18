b049073c568fa46197a132ccfe89244b57756638
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-18 00:22:04 +0000

    worked on circle generator

--- .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
+++ .ccls-cache/@home@runner@Main-Repl-Project-3/main.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <cmath>
 
 using namespace std;
 
-
-
-vector<int> gen_primes(int range_ub, int range_lb) {
-  
-  vector<int> primes;
-  
-  for (int i = range_lb; i < range_ub; i++) {
-    vector<int> factors;
-    
-    for (int j = 1; j <= i; j++) {
-      
-      if (i % j == 0) {
-        factors.push_back(j);
-      }
-    }
-
-    if (factors.size() == 2) {
-      primes.push_back(i);
-      
-    }
-    factors = {};
-  }
-
-  return primes;
-}
-
 int main() {
-  srand ( time(NULL) );
-  
-  int random = (1 + rand() % 10) * 5;
-  cout << random;
-  cout << "working!";
-  
-  
+  int radius = 9;
+  int threshold = 5;
+
+  for (int i = radius; i >= -radius; i--) {
+    float percent = ((float)(i * i) / float(81));
+
+    if (i == abs(radius)) {
+      string line(threshold, "#");
+      cout << line;
+    }
+    
+    cout << percent << "\n";
+  }
 }
