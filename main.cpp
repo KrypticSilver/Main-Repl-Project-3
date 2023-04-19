fdcca655be8e7daeddce3d884b6363736921448d
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-19 00:26:58 +0000

    worked on shape generation

--- main.cpp
+++ main.cpp
@@ -4,18 +4,42 @@
 
 using namespace std;
 
+// x = (r^2 - y^2)^(1/2)
+
 int main() {
-  int radius = 9;
-  int threshold = 5;
+  int radius = 10;
+  int diameter = radius * 2;
+  int correctionFactor = radius / 2;
+  
+  bool filledIn = false;
+
+  int initDist = sqrt(radius * radius - (radius - 1) * (radius - 1));
 
   for (int i = radius; i >= -radius; i--) {
-    float percent = ((float)(i * i) / float(81));
+    int xDist = sqrt(radius * radius - i * i);
 
-    if (i == abs(radius)) {
-      string line(threshold, "#");
-      cout << line;
+    
+    if (abs(i) == radius) {
+      cout <<
+      string((diameter - initDist) * correctionFactor + 1, ' ')
+      << 
+      string(initDist * correctionFactor * 2, '#')
+      << 
+      string((diameter - initDist), ' ') << "\n";
+      
+    } else {
+    
+      cout << 
+      string((diameter - xDist) * correctionFactor, ' ')
+      << '#' << 
+      string((xDist) * correctionFactor * 2, ' ')
+      << '#' << 
+      string((diameter - xDist), ' ') << "\n";
     }
     
-    cout << percent << "\n";
+    
   }
 }
+
+
+//TODO: work out how correction factor varies with radius, and understand properly how the inside length works
