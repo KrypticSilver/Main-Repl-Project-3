ebb74a5757a77c809a7e5dc3b39bb8ab1c269025
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-20 11:27:16 +0000

    worked on additional noise algo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int numOfSeeds = 16;
+  int lineWidth = 38;
+  int height = 25;
+
+  int seeds[numOfSeeds][2];
+  
+}
